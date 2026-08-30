e5e7ea86628c6c2bd480611a87c066c60f002b26
menchw <mainchar0@163.com>
2026-08-30 21:07:41 +0800

    perf: 更新示例代码并alert替换成console

--- src/plugins/docx/utils.ts
+++ src/plugins/docx/utils.ts
@@ -9,7 +9,7 @@ export function saveAs(blob: Blob, name: string) {
     console.warn('[saveAs] 导出word失败:', e)
     const newWin = window.open(url, '_blank')
     if (!newWin) {
-      alert(
+      console.warn(
         '【宿主沙箱提示】受系统 iframe 安全限制未能直接弹出文件保存。请在宿主 <iframe sandbox="..."> 标签中添加 allow-downloads 许可。'
       )
     }
